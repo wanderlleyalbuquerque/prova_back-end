57091ca9b8ba5eddafaeb66279d9f1e5977072f7
jose.wanderlley <110118028+wanderlleyalbuquerque@users.noreply.github.com>
2023-07-10 18:20:46 -0300

    Add files via upload

---
+++ Prova-José_Wanderley/Prova/index.js
@@ -0,0 +1,58 @@
+//quesão 01:
+
+// const http = require('http');
+// const port = 3000
+
+    //ta faltando algo...eu sei que ta, mas não lembro qual é.
+
+//     const server = http.createServer((req, res) => {
+//         res.writeHead(200, { 'Content-Type': 'application/json' });
+//         res.end()
+
+//     const objeto = [ 
+//     {id: 1, nome: 'Wanderley'},
+//     {id: 2, nome: 'Gabriel'},
+//     {id: 3, nome: 'João'},
+// ]
+// });
+// server.listen(`Servidor rodando ${porta}`)
+
+// const objeto = [ 
+//     {id: 1, nome: 'Wanderley'},
+//     {id: 2, nome: 'Gabriel'},
+//     {id: 3, nome: 'João'},
+// ]
+
+//-----------------------------------------------------------------
+
+//questão 02:
+
+// const url = require('url')
+
+// const url = 'https://al.senai.br/cursos/tecnico-em-informatica-para-internet/planodecurso.pdf'
+
+//------------------------------------------------------------------------
+
+
+
+//questão 03: 
+
+
+
+//----------------------------------------------------------
+//qustao 04:
+
+// const fs  = require('fs'); 
+
+// fs.rename('oldFile.txt ' , 'arquivorenomeado.txt' , (err)=>{
+//     if(err) throw err; 
+//     console.log('arquivo renomado')
+// });
+
+//------------------------------------------------------------
+
+//questão 05:
+
+
+
+
